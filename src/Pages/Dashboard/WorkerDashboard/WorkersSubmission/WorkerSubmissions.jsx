b8ca5bcb5d003af7bc1ad99eded66d077a976707
Sajnin14 <sajninsaima@gmail.com>
2025-01-25 11:29:28 +0600

    pagination done in my submission page of a worker

--- src/Pages/Dashboard/WorkerDashboard/WorkersSubmission/WorkerSubmissions.jsx
+++ src/Pages/Dashboard/WorkerDashboard/WorkersSubmission/WorkerSubmissions.jsx
@@ -2,18 +2,62 @@ import { useEffect, useState } from "react";
 import useAxiosSecure from "../../../../hooks/useAxiosSecure";
 import SectionTitle from "../../../../Sections/SectionTitle/SectionTitle";
 import useUser from "../../../../hooks/useUser";
+import './workerSubmission.css'
 
 const WorkerSubmissions = () => {
     const [userInfo] = useUser();
     const axiosSecure = useAxiosSecure();
     const [submissions, setSubmissions] = useState([]);
-
+    const [currentPage, setCurrentPage] = useState(0);
+    const [count, setCount] = useState('');
+    const [itemsPerPage, setItemsPerPage] = useState(10);
+    
+    useEffect(() => {
+        axiosSecure.get(`/submitTask?email=${userInfo.email}&page=${currentPage}&size=${itemsPerPage}`)
+            .then(res => {
+                setSubmissions(res.data);
+            })
+    }, [axiosSecure, userInfo, currentPage, itemsPerPage])
+    
     useEffect(() => {
         axiosSecure.get(`/submitTask?email=${userInfo.email}`)
             .then(res => {
-                setSubmissions(res.data)
+                
+                
+                const data = res.data;
+                setCount(data.length);
+                
             })
     }, [axiosSecure, userInfo])
+
+    
+    
+
+    const numberOfPages = Math.ceil(count / itemsPerPage);
+    // const numberOfPages = Math.ceil(anotherCount/itemsPerPage);
+
+    const pages = [...Array(numberOfPages).keys()];
+     
+    const handlePagination = e => {
+        e.preventDefault();
+        const value = parseInt(e.target.value);
+        setItemsPerPage(value);
+        setCurrentPage(0);
+    }
+
+    const handlePrevious = () => {
+        if (currentPage > 0) {
+            setCurrentPage(currentPage - 1);
+        }
+
+    }
+
+    const handleNext = () => {
+        if (currentPage < pages.length - 1) {
+            setCurrentPage(currentPage + 1);
+        }
+    }
+
 
     return (
         <div>
@@ -31,6 +75,7 @@ const WorkerSubmissions = () => {
                         </tr>
                     </thead>
                     <tbody>
+
                         {
                             submissions.map((submit, idx) => <tr key={idx}>
                                 <th>{idx + 1}</th>
@@ -45,7 +90,21 @@ const WorkerSubmissions = () => {
                 </table>
             </div>
 
-
+         <div className="pagination">
+            <p>currentPage : {currentPage}</p>
+            <button onClick={handlePrevious}>Previous</button>
+            {
+                pages.map((page, idx) => <button className={currentPage === page && 'selected'} onClick={() => setCurrentPage(page)} key={idx}>{page}</button>)
+            }
+            <button onClick={handleNext}>Next</button>
+            <select value={itemsPerPage} onChange={handlePagination}>
+                    <option value="3">3</option>
+                    <option value="5">5</option>
+                    <option value="10">10</option>
+                    <option value="20">20</option>
+                    <option value="50">50</option>
+                </select>
+         </div>
         </div>
     );
 };
