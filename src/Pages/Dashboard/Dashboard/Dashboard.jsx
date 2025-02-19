2b3b202cb4a21d2975a742628b895e640d031951
Sajnin14 <sajninsaima@gmail.com>
2025-02-19 19:55:19 +0600

    users image, email added in dashboard

--- src/Pages/Dashboard/Dashboard/Dashboard.jsx
+++ src/Pages/Dashboard/Dashboard/Dashboard.jsx
@@ -1,9 +1,11 @@
 
 import { Helmet } from "react-helmet";
 import SectionTitle from "../../../Sections/SectionTitle/SectionTitle";
-import dashImg from '../../../assets/images/woman-carrying-coin-icons.jpg'
+// import dashImg from '../../../assets/images/woman-carrying-coin-icons.jpg'
+import useUser from "../../../hooks/useUser";
 
 const Dashboard = () => {
+    const [userInfo] = useUser();
     return (
         <div>
             <Helmet>
@@ -11,8 +13,11 @@ const Dashboard = () => {
             </Helmet>
             <SectionTitle heading='Welcome! Explore the Routes' subHeading='You can manage your things' ></SectionTitle>
             
-            <div>
-                <img src={dashImg} />
+            <div className="text-center">
+                <h3 className="text-3xl font-bold text-center uppercase">
+                    {userInfo.name}</h3>
+                    <p className="my-5 font-semibold">{userInfo.email}</p>
+                <img src={userInfo.photo} className="mx-auto w-full"/>
             </div>
             
         </div>
